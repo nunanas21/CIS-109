f51680447beebb5b1664e2823be73ab3852319d6
nunanas21 <nam.ly21@gmail.com>
2019-10-09 20:20:28 -0700

    Project 3-Write Code for load event handler

--- MyScreenSaver/MyScreenSaver/Form1.cs
+++ MyScreenSaver/MyScreenSaver/Form1.cs
@@ -37,7 +37,25 @@ namespace MyScreenSaver
 
         private void frmScSaver_Load(object sender, EventArgs e)
         {
+            string[] images = System.IO.Directory.GetFiles("pics");// put a list of file names in the pics directory into images variable
+            
+            foreach (string image in images) 
+            {
+                BGImages.Add(new Bitmap(image));// creates a new bitmap picture and add it to the bgimage array
+            }
 
+            for (int i = 0; i < 50; ++i) // as long as i is less than 50, it will keep on increasing by 1. It will look into the directory for images and it will check as long as the images is less than 50 it will keep adding images to the screen
+            {
+                BritPic mp = new BritPic(); //
+                mp.PicNum = i % BGImages.Count;
+                mp.X = rand.Next(0, Width);
+                mp.Y = rand.Next(0, Height);
+
+                
+
+                BritPics.Add(mp); // will add and object to the end of list
+           
+            }
         }
     }
 }
