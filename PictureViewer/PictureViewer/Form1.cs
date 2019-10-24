4609cb32ae5451ff6bbb295620b33e58dfd250b8
nunanas21 <nam.ly21@gmail.com>
2019-10-23 17:42:15 -0700

    Project 5-Write code for additional buttons and event handlers

--- PictureViewer/PictureViewer/Form1.cs
+++ PictureViewer/PictureViewer/Form1.cs
@@ -32,22 +32,26 @@ namespace PictureViewer
 
         private void setBckgrndcolorButton_Click(object sender, EventArgs e)
         {
-
+            if (colorDialog1.ShowDialog() == DialogResult.OK)
+                pictureBox1.BackColor = colorDialog1.Color;
         }
 
         private void clearPicturebutton_Click(object sender, EventArgs e)
         {
-
+            pictureBox1.Image = null; 
         }
 
         private void checkBox1_CheckedChanged(object sender, EventArgs e)
         {
-
+            if (checkBox1.Checked)
+                pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage; // if check box is checked, you can stretch the size of the image
+            else
+                pictureBox1.SizeMode = PictureBoxSizeMode.Normal; // if not checked, it will go back to its normal size
         }
 
         private void closeButton_Click(object sender, EventArgs e)
         {
-
+            this.Close(); 
         }
     }
 }
