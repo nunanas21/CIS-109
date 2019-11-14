a3eee128e27365173386dc95023ba7ef3a18ec93
nunanas21 <nam.ly21@gmail.com>
2019-11-13 20:11:39 -0800

    digital clock

---
+++ digitalClock.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace digitalClock
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            timer1.Start(); //calling the timer from the form. Once the form loads, it will trigger or call the method to start
+        }
+  
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            hourMinuteLabel.Text = DateTime.Now.ToString("HH:mm"); //dot. means its a method and it takes in text. Text is set to take in the datetime.now method. This method witll take the current date time from the computer and dipslay it by converting it into a string in the specified format. 
+            secondsLabel.Text = DateTime.Now.ToString("ss");
+            dateLabel.Text = DateTime.Now.ToString("MMM:dd:yyyy");
+            weekdayLabel.Text = DateTime.Now.ToString("dddd");
+
+        }
+
+    }
+}
--- simpleClock/simpleClock/Form1.cs
+++ simpleClock/simpleClock/Form1.cs
@@ -12,14 +12,35 @@ namespace simpleClock
 {
     public partial class Clock : Form
     {
+        Timer t = new Timer();
         public Clock()
         {
             InitializeComponent();
-        }
+        } 
 
         private void label1_Click(object sender, EventArgs e)
         {
+            t.Interval = 1000;
 
+            t.Tick += new EventHandler(this.t_Tick);
+
+            t.Start(); //once forms loads, it will start the timer
         }
-    }
+        private void t_Tick(object sender, EventArgs e)
+        {
+            int hh = DateTime.Now.Hour; // this wil retrieve whatever current time is on the computer
+            int mm = DateTime.Now.Minute;
+            int ss = DateTime.Now.Second;
+
+            string time = "";
+            if (hh < 10)
+            {
+                time += "0" + hh;
+            }
+            else
+            {
+                time += hh; 
+            }
+            time += ":";
+        }
 }
